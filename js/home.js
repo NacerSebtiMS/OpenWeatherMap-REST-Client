f46cd32999697c1d1d6c6d46d4ccdcbbcd0975d1
rickytechnip <rickytechnip@gmail.com>
2020-12-29 14:02:08 -0500

    add showCurrentWeather function

---
+++ js/home.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    showCurrentWeather(36513);
+});
+
+function showCurrentWeather(zipcode, units="imperial", courtryCode="us") {
+    $('#errorMessages').empty();
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.openweathermap.org/data/2.5/weather?' + 'zip=' + zipcode + ',' + courtryCode + '&appid=3f26ae1206a31ead737823b919e97c08' + '&units=' + units,
+        success: function(data, status) {
+            $('#cityName').html(data.name);
+            $('#currentWeatherDiscription').html(data.weather[0].description);
+            $('#currentTemperature').html(data.main.temp);
+            $('#currentHumidity').html(data.main.humidity);
+            $('#currentWind').html(data.wind.speed);
+            $('#currentWeatherImage').attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+        },
+        error: function() {
+            $('#errorMessages')
+                .append($('<li>')
+                    .attr({class: 'list-group-item list-group-item-danger'})
+                    .text('Error calling web service. Please try again later.'));
+        }
+    });
+    $('#currentWeatherFormDiv').show();
+}
